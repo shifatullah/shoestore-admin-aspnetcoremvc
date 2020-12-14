c22b9c392055d4beef393efdc557899765202f61
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2020-12-15 06:38:46 +1100

    new asp.net core mvc project using visual studio

---
+++ ShoeStore.Admin.AspNetCoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShoeStore.Admin.AspNetCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
